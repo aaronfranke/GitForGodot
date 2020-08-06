6e3e8a1866c365bae6be24e5c90cf24031a9794f
Aaron Franke <arnfranke@yahoo.com>
2020-08-05 22:29:36 -0500

    Add staging dock
    Also move commit name to the correct folder

--- addons/git_for_godot/plugin.gd
+++ addons/git_for_godot/plugin.gd
@@ -4,14 +4,17 @@ extends EditorPlugin
 
 const MainScreen = preload("res://addons/git_for_godot/history_main_screen/history_main_screen.tscn")
 const CommitDock = preload("res://addons/git_for_godot/commit_dock/commit_dock.tscn")
+const StagingDock = preload("res://addons/git_for_godot/staging_dock/staging_dock.tscn")
 
 var main_screen_instance
 var commit_dock_instance
+var staging_dock_instance
 
 
 func _enter_tree():
 	main_screen_instance = MainScreen.instance()
 	commit_dock_instance = CommitDock.instance()
+	staging_dock_instance = StagingDock.instance()
 	# Add the main panel to the editor's main viewport.
 	get_editor_interface().get_editor_viewport().add_child(main_screen_instance)
 	# Hide the main panel. Very much required.
@@ -33,9 +36,11 @@ func has_main_screen():
 func make_visible(visible):
 	if visible:
 		add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_BL, commit_dock_instance)
+		add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, staging_dock_instance)
 		hide_bottom_panel()
 	else:
 		remove_control_from_docks(commit_dock_instance)
+		remove_control_from_docks(staging_dock_instance)
 	if main_screen_instance:
 		main_screen_instance.visible = visible
 
